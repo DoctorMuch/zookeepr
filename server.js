58f92f772dcfa301a4e359b89a1816f85c27f64f
David F Mueth <davidmueth@gmail.com>
2021-10-11 02:14:19 -0500

    Set up query and param

--- server.js
+++ server.js
@@ -31,6 +31,11 @@ function filterByQuery(query, animalsArray) {
     filteredResults = filteredResults.filter(animal => animal.name === query.name);
   }
   return filteredResults;
+};
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id[0]);
+  return result;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -38,6 +43,15 @@ app.get('/api/animals', (req, res) => {
   console.log(req.query);
   res.json(results);
 });
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
