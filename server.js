76764b8fb256a2140cf81783593cf969bf537c51
David F Mueth <davidmueth@gmail.com>
2021-10-13 00:29:03 -0500

    POST and validation

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-const app = express();
 
-const { animals } = require('./data/animals.json')
+const app = express();
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+const { animals } = require('./data/animals');
+const { type } = require('os');
 
 function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
   let filteredResults = animalsArray;
   if (query.personalityTraits) {
     //Save personalityTraits as dedicated array.
@@ -36,13 +44,53 @@ function filterByQuery(query, animalsArray) {
 function findById(id, animalsArray) {
   const result = animalsArray.filter(animal => animal.id === id[0]);
   return result;
+};
+
+function createAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+};
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !=='string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !=='string') {
+    return false;
+  }
+  if (!animal.personalityTraits || typeof animal.personalityTraits !== 'string') {
+    return false;
+  }
+  return true;
 }
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
-  console.log(req.query);
+ if (req.query) {
+   results = filterByQuery(req.query, results);
+ }
   res.json(results);
 });
+
+app.post('/api/animals', (req, res) => {
+  req.body.id = animals.length.toString();
+
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
 app.get('/api/animals/:id', (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
